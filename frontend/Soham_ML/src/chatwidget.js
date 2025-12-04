f1d6b621922684142fc58a0de6129e9e1c4e46fa
MD SAHIL <144834170+mdsahilnoob@users.noreply.github.com>
2025-12-05 02:12:27 +0530

    fix: cors error for ml backend

--- frontend/Soham_ML/src/chatwidget.js
+++ frontend/Soham_ML/src/chatwidget.js
@@ -33,7 +33,7 @@ const ChatWidget = () => {
     setInput("");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/ask", {
+      const res = await fetch("https://my-ml-api-z4zs.onrender.com/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: userMessage }),
